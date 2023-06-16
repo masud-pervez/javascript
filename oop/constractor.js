7507fd506aa894bd5d65cb2de907cfb0a682cbb6
gowtamkumar <gowtampaul0@gmail.com>
2023-06-16 07:50:49 +0600

    constractor-pattern

---
+++ oop/constractor.js
@@ -0,0 +1,13 @@
+function Person(fName, lName) {
+  this.fName = fName;
+  this.lName = lName;
+
+  this.fullName = function () {
+    return this.fName + " " + this.lName;
+  };
+}
+
+const res = new Person("gow", "pal")
+
+console.log(res);
+
