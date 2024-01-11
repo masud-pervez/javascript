6471f5a38b0f46df2df716a1b4e564fa70f287b4
gowtamkumar <gowtampaul0@gmail.com>
2024-01-11 21:26:35 +0600

    node core

--- node/index.js
+++ node/index.js
@@ -1,14 +1,13 @@
 const http = require("http");
-const buffer =  Buffer.from(8); 
-const buffer2 =  Buffer.alloc(4); 
-console.log("🚀 ~ buffer:", buffer.write("ee", 1))
+const dns =  require('dns') 
+console.log("🚀 ~ buffer:", dns.lookupService())
 
 
-const createserver = http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h2>This is a nodejs servezz</h2>");
-  })
-  .listen(3000, () => console.log("Server running successfully"));
+// const createserver = http
+//   .createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     res.end("<h2>This is a nodejs servezz</h2>");
+//   })
+//   .listen(3000, () => console.log("Server running successfully"));
 
 
