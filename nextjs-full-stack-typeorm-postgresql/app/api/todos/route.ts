76c2737c2e9a1438784814b99ab142a3ee83bfd6
gowtamkumar <gowtampaul0@gmail.com>
2024-02-02 11:34:24 +0600

    typeorm remove

--- nextjs-full-stack-typeorm-postgresql/app/api/todos/route.ts
+++ nextjs-full-stack-typeorm-postgresql/app/api/todos/route.ts
@@ -1,8 +1,4 @@
-import  PostgresDatabaseConnection  from '@/db/posgresql_db';
 import { NextRequest, NextResponse } from "next/server";
-import { DataSource } from "typeorm";
-// import { DataSource } from "typeorm"
 export async function GET(request: Request) {
-  PostgresDatabaseConnection
   return NextResponse.json({ name: "Gowtamkumar" });
 }
