45de867b785066d3711e96c9ae077731d8f3e776
gowtamkumar <gowtampaul0@gmail.com>
2024-02-05 10:43:42 +0600

    division and modulus

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log(10 ** 3); //Exponentiation that mine 10 x 10 = 100 x 10 
+const resutl = Math.pow(10, 3);
+// console.log(resutl);
+
+console.log(10/3) //division just division kore dei;
+console.log(10%3); //vagse bar kora hoy
