ce9d48e173ee7e3f3e586e86561157d8523e05fd
gowtamkumar <gowtampaul0@gmail.com>
2024-02-07 14:34:50 +0600

    preparation for interview

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
-console.log(10 ** 3); //Exponentiation that mine 10 x 10 = 100 x 10 
+console.log(10 ** 3); //Exponentiation that mine 10 x 10 = 100 x 10
 const resutl = Math.pow(10, 3);
 // console.log(resutl);
 
-console.log(10/3) //division just division kore dei;
-console.log(10%3); //vagse bar kora hoy
+console.log(10 / 3); //division just division kore dei;
+console.log(10 % 3); //vagse bar kora hoy
+console.log(3 && 3);
+
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  default:
+    day = "There is no matching! sorry for than";
+}
+
+// console.log(day);
+
+const person = { fname: "John", lname: "Doe", age: 25 };
+
+let text = "";
+for (let x in person) {
+  // console.log(person);
+  text += person[x];
+}
+
+// console.log(text);
+
+const cars = "Javascript";
+
+let texts = "";
+for (let x of cars) {
+  //   console.log(x);
+  texts += x;
+}
+
+let i = 0;
+// while (i < 10) {
+//     // console.log(i);
+//     i++;
+// }
+
+do {
+    i++;
+  console.log("aaa",i);
+} while (i < 10);
---
+++ interview-question/prepare-for-software-engineering-foyzul.md
@@ -0,0 +1,27 @@
+## how to prepare for software engineering interview?
+
+# topics
+
+    1.phases
+    2.roles
+    3.countries/companies
+
+# steps
+
+    1.cv + profile,
+    2.screening,
+    3.project/online testing,
+    4.technical interview,
+    5.behavioural interview
+    6.offer
+
+# importent
+
+    linkedin, github,cv
+
+    # introduce yourself
+    # fluent speaking,
+
+    need to inproved cv, linkein, english, and technical knowledge, probling solving, online testing, communication
+
+reactjs, node js and framwork in details knowldege
